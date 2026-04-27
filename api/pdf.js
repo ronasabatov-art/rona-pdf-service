ff04916501b0e0f3d6ecd6c55cde84afa32a254b
ronasabatov-art <ronasabatov@gmail.com>
2026-04-27 11:11:35 +0300

    Refactor PDF generation to use ES6 imports

--- api/pdf.js
+++ api/pdf.js
@@ -1,5 +1,5 @@
-const chromium = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer-core');
+import chromium from "@sparticuz/chromium";
+import puppeteer from "puppeteer-core";
 
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -11,26 +11,31 @@ export default async function handler(req, res) {
   try {
     const { html } = req.body;
 
+    // כאן אנחנו אומרים לשרת להוריד את ה"מנוע" של הדפדפן אם הוא חסר
+    const executablePath = await chromium.executablePath(
+      `https://github.com/sparticuz/chromium/releases/download/v123.0.1/chromium-v123.0.1-pack.tar`
+    );
+
     const browser = await puppeteer.launch({
-      args: chromium.args,
-      executablePath: await chromium.executablePath,
+      args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chromium.defaultViewport,
+      executablePath: executablePath,
       headless: chromium.headless,
     });
 
     const page = await browser.newPage();
-    await page.setContent(html, { waitUntil: 'networkidle0' });
-    
+    await page.setContent(html, { waitUntil: "networkidle0" });
+
     const pdf = await page.pdf({
-      format: 'A4',
+      format: "A4",
       printBackground: true,
-      margin: { top: '0px', right: '0px', bottom: '0px', left: '0px' }
+      margin: { top: "0px", right: "0px", bottom: "0px", left: "0px" }
     });
 
     await browser.close();
-
-    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader("Content-Type", "application/pdf");
     res.send(pdf);
   } catch (error) {
-    res.status(500).send("Error: " + error.message);
+    res.status(500).send("Server Error: " + error.message);
   }
 }
